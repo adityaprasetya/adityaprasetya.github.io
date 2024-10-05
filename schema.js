65c64286f7882d4fff6cf79176370d2b618bc897
Aditya Prasetya Suryana <adityaprasetyasuryana@gmail.com>
2024-10-05 21:57:57 +0700

    Create schema.js

---
+++ schema.js
@@ -0,0 +1,19 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "Aditya Prasetya Suryana",
+  "url": "https://adityaprasetya.github.io",
+  "logo": "https://adityaprasetya.github.io/logo.jpeg",
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "+62-81224603966",
+    "contactType": "Customer Service"
+  },
+  "sameAs": [
+    "https://www.facebook.com/nason.suryana",
+    "https://www.x.com/adityapras82",
+    "https://www.linkedin.com/in/aditya-prasetya-suryana"
+  ]
+}
+</script>
